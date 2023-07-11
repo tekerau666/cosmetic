bc14c6651f85fd194c1a8dbdfcf400de7fff339f
tekerau666 <tekerau666@mail.ru>
2023-07-11 14:33:27 +0400

    added css loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,6 +2,15 @@ import webpack from "webpack";
 
 
 export function buildLoaders(): webpack.RuleSetRule[ ] {
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+        ]
+    }
+
     const typescriptLoader =  {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -9,5 +18,6 @@ export function buildLoaders(): webpack.RuleSetRule[ ] {
     }
     return [
        typescriptLoader,
+        cssLoader,
     ]
 }

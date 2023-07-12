f3623536e19d1083b5e0f8db39b859ea2cdad6bc
PRIZEOX <oganisyanararat1@gmail.com>
2023-07-12 20:52:18 +0400

    Some updates in styles

--- src/componets/Hero/Hero.tsx
+++ src/componets/Hero/Hero.tsx
@@ -1,15 +1,22 @@
 import style from './Hero.module.scss'
-
-import { Image, Text } from '@nextui-org/react';
+import logo from '../../assets/svg/Vector.svg'
+import { Image, Text} from '@nextui-org/react';
 import React from 'react';
 
 
 function Hero(){
+    const text: string = 'There are many variation of passages are Ipsum available, majority have suffered alteration in some form.';
     return(
-        <body>
-
-        </body>
-
+        <div className={style.back}>
+            <div className={style.content}>
+                <div className={style.info}>
+                    <img src={logo} alt=""/>
+                    <p>Hair Salon, Masseuse, Beauty Salon</p>
+                </div>
+                <Text size={70} h1 color="#F7E5C1">Find a service</Text>
+                <Text size ={70} h1 color="#F7E5C1">close to you</Text>
+            </div>
+        </div>
     );
 }
 

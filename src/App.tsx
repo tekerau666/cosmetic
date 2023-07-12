a7ac86fe4fe37fd5b67d265c3a73591e4fdad528
tekerau666 <tekerau666@mail.ru>
2023-07-12 13:37:13 +0400

    pages: add routing and suspense for Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Suspense} from "react";
+import {Spinner} from "@nextui-org/react";
 
 function App() {
-  return (
-   <p>
-
-   </p>
-  );
+    return (
+        <div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
 }
 
 export default App;

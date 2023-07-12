4f31ec0c54d6f8e9fdb8e0b4b3d2bdb3dd864abe
PRIZEOX <oganisyanararat1@gmail.com>
2023-07-12 21:52:29 +0400

    Почти закончил Hero

--- src/componets/Hero/Hero.tsx
+++ src/componets/Hero/Hero.tsx
@@ -6,16 +6,33 @@ import React from 'react';
 
 function Hero(){
     const text: string = 'There are many variation of passages are Ipsum available, majority have suffered alteration in some form.';
+/*    const searcherElement=() =>{
+        return(
+            <>
+
+            </>
+        );
+    }*/
     return(
         <div className={style.back}>
             <div className={style.content}>
                 <div className={style.info}>
-                    <img src={logo} alt=""/>
-                    <p>Hair Salon, Masseuse, Beauty Salon</p>
+                    <img src={logo}/>
+                    <Text transform='uppercase'  color='white' size={15}>Hair Salon, Masseuse, Beauty Salon</Text>
                 </div>
                 <Text size={70} h1 color="#F7E5C1">Find a service</Text>
                 <Text size ={70} h1 color="#F7E5C1">close to you</Text>
+                <div className={style.ps}>
+                    <Text size={18} color='white'>There are many variation of passages are Ipsum available,</Text>
+                    <Text size={18} color='white'>majority have suffered alteration in some form.</Text>
+                </div>
+                <div className={style.searcher}>
+                    <div className={style.searcher_container}>
+
+                    </div>
+                </div>
             </div>
+            <div className={style.last_pading}></div>
         </div>
     );
 }

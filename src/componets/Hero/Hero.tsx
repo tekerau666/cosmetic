2275751a3a5839749aacb3b0bf4a8703ea60a3f1
PRIZEOX <oganisyanararat1@gmail.com>
2023-07-15 18:40:18 +0400

    Закончил Hero компонент

--- src/componets/Hero/Hero.tsx
+++ src/componets/Hero/Hero.tsx
@@ -1,23 +1,19 @@
 import style from './Hero.module.scss'
-import logo from '../../assets/svg/Vector.svg'
-import { Image, Text} from '@nextui-org/react';
+import logoImg from '../../assets/svg/Vector.svg'
+import locationImg from '../../assets/svg/location.svg'
+import searchImg from '../../assets/svg/search.svg'
+import secondSearchImg from '../../assets/svg/whiteSearch.svg'
+import { Text} from '@nextui-org/react';
 import React from 'react';
 
 
+
 function Hero(){
-    const text: string = 'There are many variation of passages are Ipsum available, majority have suffered alteration in some form.';
-/*    const searcherElement=() =>{
-        return(
-            <>
-вфы
-            </>
-        );
-    }*/
     return(
         <div className={style.back}>
             <div className={style.content}>
                 <div className={style.info}>
-                    <img src={logo}/>
+                    <img alt="Лого" src={logoImg}/>
                     <Text transform='uppercase'  color='white' size={15}>Hair Salon, Masseuse, Beauty Salon</Text>
                 </div>
                 <Text size={70} h1 color="#F7E5C1">Find a service</Text>
@@ -28,7 +24,24 @@ function Hero(){
                 </div>
                 <div className={style.searcher}>
                     <div className={style.searcher_container}>
-
+                        <div className={style.searcher_elem}>
+                            <h4>Service Name</h4>
+                            <div className={style.searcher_elem_content}>
+                                <text>Book your services...</text>
+                                <img src={searchImg} alt="Search Picture"/>
+                            </div>
+                        </div>
+                        <div className={style.searcher_elem}>
+                            <h4>Address</h4>
+                            <div className={style.searcher_elem_content}>
+                                <text>Where</text>
+                                <img src={locationImg} alt="Location Picture"/>
+                            </div>
+                        </div>
+                        <button className={style.searcher_button_content}>
+                            <a>Search</a>
+                            <img  src={secondSearchImg} alt="Search!"/>
+                        </button>
                     </div>
                 </div>
             </div>

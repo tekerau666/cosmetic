20eb4efa63a64a7bfaa21c0b565f2a7d66953d99
tekerau666 <tekerau666@mail.ru>
2023-07-24 20:45:26 +0400

    Add custom Loader.

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import './Loader.scss'
+import {FC} from "react";
+import {classNames} from "shared/lib/classNames/classNames"
+
+
+interface LoaderProps {
+    className?: string,
+}
+
+export const Loader: FC<LoaderProps> = ({children, className}) => {
+    return (
+        <div
+            className={classNames('loader', {}, [className])}
+        >
+            <span className="loader"></span>
+        </div>
+    )
+}

51057a1089e4392dda1469e8c953cc5558aa4b19
tekerau666 <tekerau666@mail.ru>
2023-07-26 17:58:22 +0400

    love joskiy vibe

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json",
+        // "tsconfigRootDir": __dirname
+    },
+    "plugins": [
+        "react",
+        ["i18next"]
+    ],
+    "rules": {
+        "@typescript-eslint/explicit-function-return-type": "warn",
+        "@typescript-eslint/prefer-nullish-coalescing": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "react/react-in-jsx-scope": "off",
+        "@typescript-eslint/no-misused-promises": "warn",
+        "@typescript-eslint/no-floating-promises": "warn",
+        "@typescript-eslint/ban-ts-comment": "warn",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/naming-convention": "warn",
+        "i18next/no-literal-string": "error",
+    },
+    globals: {
+        '__IS_DEV__': true,
+    }
+}

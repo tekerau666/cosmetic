f5e518bb5cf504d1fb45354d9c5cae0db126e571
tekerau666 <tekerau666@mail.ru>
2023-08-11 21:43:42 +0400

    memoized

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,7 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './AppLink.module.scss'
 import {Link, type LinkProps} from 'react-router-dom'
-import { type FC } from 'react'
+import {type FC, memo, ReactNode} from 'react'
 
 export enum AppLinkTheme {
   PRIMARY = 'primary',
@@ -10,11 +10,12 @@ export enum AppLinkTheme {
 }
 
 interface AppLinkProps extends LinkProps {
-  className?: string
-  theme?: AppLinkTheme
+  className?: string;
+  theme?: AppLinkTheme;
+  children: ReactNode;
 }
 
-export const AppLink: FC<AppLinkProps> = (props) => {
+export const AppLink = memo(( props: AppLinkProps) => {
   const {
     to,
     className,
@@ -32,4 +33,4 @@ export const AppLink: FC<AppLinkProps> = (props) => {
           {children}
         </Link>
   )
-}
+})
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Button.module.scss'
-import { type ButtonHTMLAttributes, type FC } from 'react'
+import {type ButtonHTMLAttributes, type FC, memo, ReactNode} from 'react'
 
 export enum ButtonTheme {
   CLEAR = 'clear',
@@ -11,11 +11,12 @@ export enum ButtonTheme {
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  className?: string
-  theme?: ButtonTheme,
-  square?: boolean,
-  size?: ButtonSize,
-  disabled?: boolean
+  className?: string;
+  theme?: ButtonTheme;
+  square?: boolean;
+  size?: ButtonSize;
+  disabled?: boolean;
+  children: ReactNode;
 }
 
 export enum ButtonSize {
@@ -24,7 +25,7 @@ export enum ButtonSize {
   XL = 'size_xl',
 }
 
-export const Button: FC<ButtonProps> = (props) => {
+export const Button = memo((props: ButtonProps) => {
   const {
     className,
     children,
@@ -50,4 +51,4 @@ export const Button: FC<ButtonProps> = (props) => {
             {children}
         </button>
   )
-}
+})
